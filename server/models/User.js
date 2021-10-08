0e1042153fcc18fe1a757391410dd6d738aafa1b
Lucas Boinet <42383760+lucasboinet@users.noreply.github.com>
2021-10-08 16:59:10 +0200

    Create User.js

---
+++ server/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    coins: {
+        type: Integer,
+        required: true
+    },
+    registered_at: {
+        type: Date,
+    },
+    updated_date: {
+        type: Date,
+        default: Date.now
+    }
+  });
+  
+  module.exports = User = mongoose.model('user', UserSchema);
