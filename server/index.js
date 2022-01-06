c0764a4eefbc1e74832dc6cfa212622f7431e2ec
Lucas Boinet <42383760+lucasboinet@users.noreply.github.com>
2022-01-07 00:36:21 +0100

    cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const session = require('express-session')
+const cors = require('cors');
 
 const league = require('./routes/league');
 const users = require('./routes/users');
@@ -10,7 +11,6 @@ const friends = require('./routes/friends');
 
 
 const app = express();
-const cors = require('cors');
 
 const cors_options = {
   origin: true
@@ -19,12 +19,16 @@ const cors_options = {
 connectDB();
 
 app.use(express.static('public'))
+
 app.use(cors(cors_options));
 app.options('*', cors(cors_options))
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 app.use(express.json({ extended: false }));
 
 app.use(
