263cb6783142e73b5aef63a998e28696cfcd3eec
Lucas Boinet <42383760+lucasboinet@users.noreply.github.com>
2022-01-07 00:45:58 +0100

    cors

--- server/index.js
+++ server/index.js
@@ -20,8 +20,10 @@ app.use(cors());
 app.options('*', cors())
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+	res.setHeader('Access-Control-Allow-Credentials', true)
   next();
 });
 
